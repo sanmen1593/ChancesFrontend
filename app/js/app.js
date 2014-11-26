0098864e320488508e381c228915610df48a9ad3
andrescoulson <andresbobadillacoulson@gmail.com>
2014-11-26 12:43:00 -0500

    Revert "controller  vehicle"
    This reverts commit e9d29eea6adec78f846c747a2d52e2af1f178646.

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,38 @@
-(function() {
-    var app = angular.module('chanceApp', [
-        'app.directives',
-        'app.controllers'
-    ]);
+(function(){
+   var app = angular.module('chanceutb', [
+       'ngRoute',
+       'app.directives',
+       'app.controllers',
+       'ngRoute'
+   ])
+    app.config(function ( $routeProvider ) {
 
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/chances-front.html'
+            })
+            .when('/create_vechicle', {
+                templateUrl: 'views/create_vehicle.html'
+            })
+            .when('/profile-vehicles', {
+                templateUrl: 'views/profile_vehicles.html'
+            })
+            .when('/profile-chance', {
+                templateUrl: 'views/profile_chance.html'
+            })
+            .otherwise( {
+                redirectTo: '/'
+            })
 
-    app.config(['$routerProvider', function($routeProvider) {
-
-            $routeProvider.when('session', {
-                url: "/",
-                templateUrl: "views/session.html",
-                controller: 'SessionController'
-            }),
-            $routeProvider.when('vehicle', {
-                url: "/vehicle",
-                templateUrl: "views/vehicle_profile.html",
-                controller: "VehicleController"
-                
-            }),
-            $routeProvider.when('user', {
-                url: "/user",
-                templateUrl: "views/user.html",
-                controller: 'UserController'
-            }),
-            $routeProvider.when('chance', {
-                url: "/chance",
-                templateUrl: "views/chance.html",
-                controller: 'ChancerController'
-            });
-
-        }]);
-
+    });
 })();
+
+
+
+
+    create_vechicle
+
+    profile-vehicles
+
+   profile-chance
 
